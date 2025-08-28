dd08680f2d7baf97025f1dec7c99ae9799154196
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-08-29 02:45:23 +0600

    Added the card section and working on js about cards

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+// emergency services data
+const services = [
+  {
+    mainName:"National Emergency Number",
+    name: "National Emergency",
+    number: "999",
+    category: "General",
+  },
+  {
+    mainName:"Police Helpline Number",
+    name: "Police",
+    number: "999",
+    category: "Law",
+  },
+  {
+    mainName:"Fire Service Number",
+    name: "Fire Service",
+    number: "999",
+    category: "Fire",
+  },
+  {
+    mainName:"Ambulance Service",
+    name: "Ambulance",
+    number: "16263",
+    category: "Health",
+  },
+  {
+    mainName:"Women & Child Helpline",
+    name: "Railway Helpline",
+    number: "131",
+    category: "Transport",
+  },
+  {
+    mainName:"Anti-Corruption Helpline",
+    name: "Anti-Corruption",
+    number: "106",
+    category: "Law",
+  },
+];
